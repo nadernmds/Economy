62bf821577c872b17e7376f34000d14d5a7af02e
nadernmds <nadernmds@gmail.com>
2019-07-24 07:57:04 +0430

    jhk

--- Models/Company.cs
+++ Models/Company.cs
@@ -12,9 +12,10 @@ namespace Economy.Models
         public int companyID { get; set; }
         public string companyName { get; set; }
         public string startDate { get; set; }
-        public virtual ICollection<CompanyUser> CompanyUsers { get; set; }
+        public virtual ICollection<CompanyUser> CompanyUsers { get; set; } = new HashSet<CompanyUser>();
         public virtual ActivityType ActivityType { get; set; }
-        public virtual ICollection<Project> Projects { get; set; }
+        public virtual ICollection<Project> Projects { get; set; } = new HashSet<Project>();
+        public virtual ICollection<Asset> Assets { get; set; } = new HashSet<Asset>();
         public virtual Owner Owner { get; set; }
     }
 }
